2c6000092d5acdb14f148ddb0b1a2c0a123cb37a
mehedy hassan <mehedy@algosolver.com>
2023-02-25 16:08:19 +0600

    feat: flowbite tailwind fixed

--- components/Layouts/SidebarCmp/index.tsx
+++ components/Layouts/SidebarCmp/index.tsx
@@ -10,7 +10,6 @@ import {
 } from "react-icons/hi";
 const SidebarCmp = () => {
   return (
-    <div className="bg-[#CCE1F2]">
   <Sidebar aria-label="Default sidebar example">
     <Sidebar.Items>
       <Sidebar.ItemGroup>
@@ -62,7 +61,6 @@ const SidebarCmp = () => {
       </Sidebar.ItemGroup>
     </Sidebar.Items>
   </Sidebar>
-    </div>
   );
 };
 

c774b2a4fb279adecab570612e4a9beb8bf6c133
mehedy hassan <mehedy@algosolver.com>
2023-02-26 01:22:45 +0600

    feat: footer responsive

--- components/Layouts/Footer/index.tsx
+++ components/Layouts/Footer/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='fixed bottom-0 w-3/4  flex gap-1 justify-center py-2'>
+    <div className='hidden md:fixed bottom-0 w-3/4  md:flex md:gap-1 md:justify-center py-2'>
         <p className='text-gray-600'>© Copyright {new Date().getFullYear()} , </p>
         <a href="https://fourbit.io" target="_blank" className='text-indigo-700 hover:text-indigo-400'>fourbit.io</a>
         <p>All Rights Reserved</p>

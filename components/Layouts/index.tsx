e49045329c1cb562bdb557bf138f0dabd65554c4
mehedy hassan <mehedy@algosolver.com>
2023-02-26 01:18:58 +0600

    feat: footer added

--- components/Layouts/index.tsx
+++ components/Layouts/index.tsx
@@ -1,3 +1,4 @@
+import Footer from "./Footer"
 import Header from "./Header"
 import SidebarCmp from "./SidebarCmp"
 
@@ -5,14 +6,13 @@ const Layout = ({children} : any) => {
   return (
     <div className="flex gap-2 w-full">
       <SidebarCmp/>
-    
     <div className="flex-1">
     <Header/>
     <div className="w-full h-screen bg-[#CCE1F2] p-4">
       {children}
     </div>
+    <Footer/>
     </div>
-    
     </div>
   )
 }

6b7bae2d139ab2db7f6d3bfd2538892049f27717
mehedy hassan <mehedy@algosolver.com>
2023-02-26 02:14:06 +0600

    feat: footer gap fixed

--- components/Dashboard/index.tsx
+++ components/Dashboard/index.tsx
@@ -2,17 +2,17 @@ import Histogram from "./Charts/Histogram";
 import Geo from "./Charts/Geo";
 import Pie from "./Charts/Pie";
 import OrderTable from "./Tables/OrderTable";
-
 const Dashboard = () => {
+
   return (
     <div className="grid grid-cols-3 gap-2 ">
       <div className="col-span-2 space-y-2">
-      <Geo/>
-      <OrderTable/>
+        <Geo />
+        <OrderTable />
       </div>
       <div className="space-y-2">
-        <Pie/>
-        <Histogram/>
+        <Pie />
+        <Histogram />
       </div>
     </div>
   );

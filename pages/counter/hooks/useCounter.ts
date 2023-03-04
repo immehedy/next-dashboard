6da2c431655b56ebd11cfb2f290db59ffdad4b2d
mehedy hassan <dev.mehedy.hassan@gmail.com>
2023-03-05 03:17:31 +0600

    feat: removed unnecessary test sripts

--- pages/counter/hooks/useCounter.ts
+++
@@ -1,8 +0,0 @@
-import { useState } from 'react';
-import { useCounterType } from './useCounter.types';
-export const useCounter = ({initialValue = 0} : useCounterType = {}) => {
-    const [count, setCount] = useState(initialValue);
-    const increment = () => {setCount(count + 1)};
-    const decrement = () => {setCount(count - 1)};
-    return {count, increment, decrement};
-}
